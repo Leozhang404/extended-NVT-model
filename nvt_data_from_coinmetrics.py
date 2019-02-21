2e56a9ce4f845e3ca33b2e3041a8bf122bbe819c
HiddenOrder <38653654+HiddenOrder@users.noreply.github.com>
2019-02-21 15:28:11 +0800

    Plot all figure to a page use pyecharts
    1、the figure is more convenient to study;
    2、I change the signal's name to NVU, which means Netwok Value over Utility;
    3、The legend's color has some problem, but it do not affect to see

--- nvt_data_from_coinmetrics.py
+++ nvt_data_from_coinmetrics.py
@@ -5,12 +5,14 @@ Created on Sat Jan 12 19:08:57 2019
 @author: Leo
 """
 
-import nvt_model as nm
+import nvt_model_pyecharts as nm
 import get_data_from_api as gdfa
 import numpy as np
+import time
+from pyecharts import Page
 
 
-data = gdfa.get_history_data()
+data = gdfa.get_history_data(token_name = 'btc',time_start = '2013-04-28 00:00:00',time_end = time.time())
 
 v_index = list(range(len(data['price(usd)'])))
 
@@ -27,34 +29,41 @@ exchangevolume =list(np.array(data['exchangevolume(usd)'])[:,1])
 tx_count =list(np.array(data['txcount'])[:,1])
 
 # activeaddresses
-activeaddresses =np.array(data['activeaddresses'])[:,1]
+activeaddresses =list(np.array(data['activeaddresses']))[:,1]
 
 marketcap_btc = list(np.array(data['marketcap(usd)'])[:,1])
 btc_price_nvt = list(np.array(data['price(usd)'])[:,1])
 
 
+utility = []
+features = ['Volume on chain', 
+            'Activeaddresses', 
+            'Volume of average activeaddresses',
+            'Volume of average transaction counts',
+            'Counts  of average activeaddresses']
 
 #volume_btc = list(np.power(activeaddresses,2))
 
 # 链上交易量+交易所交易量
 #volume_btc = [txvolume[i]+exchangevolume[i] for i in range(0,len(txvolume))] 
 
+utility.append(txvolume)
+utility.append(activeaddresses)
+
+# 平均每个活跃地址的交易量
+utility.append([txvolume[i]/activeaddresses[i] for i in range(0,len(txvolume))] )
+
 # 平均每次交易量
-#volume_btc = [txvolume[i]/tx_count[i] for i in range(0,len(txvolume))] 
+utility.append([txvolume[i]/tx_count[i] for i in range(0,len(txvolume))]) 
 
-# 平均每个活跃地址的交易量
-volume_btc = [txvolume[i]/activeaddresses[i] for i in range(0,len(txvolume))] 
-
-# 平均每个活跃地址的交易量
-#volume_btc = [activeaddresses[i] * activeaddresses[i] for i in range(0,len(txvolume))] 
-
-#volume_btc = activeaddresses
 
 # 平均每个活跃用户的 交易频次
-#volume_btc = [activeaddresses[i] / tx_count[i] for i in range(0,len(txvolume))]
+utility.append([tx_count[i]/activeaddresses[i] for i in range(0,len(txvolume))])
 
 
-
-
-
-nvt, nvt_MA = nm.cal_NVT_plot(v_index, volume_btc, marketcap_btc, ts_btc, btc_price_nvt, ma_volume=30, ma_nvt=90)
+page = Page(page_title = 'NVT Signal')
+for i in range(len(utility)):
+    chart_title = "NVT with {}".format(features[i])
+    overlap = nm.cal_NVT_plot(chart_title, v_index, utility[i], marketcap_btc, ts_btc, btc_price_nvt, ma_volume=30, ma_nvt=90)
+    page.add(overlap)
+page.render("my_NVU.html")
